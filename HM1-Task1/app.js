145777a177143645e282a6f948eb7bce8d0a658d
andreirysh <andreirysh@gmail.com>
2023-08-01 20:29:30 +0200

    update logic

--- HM1-Task1/app.js
+++ HM1-Task1/app.js
@@ -8,21 +8,14 @@ function isNumValid(number) {
 
 function convertNumberToBase() {
     const num = prompt('Введите число');
-
-    if (!isNumValid(num)) {
-        console.log('Некорректный ввод!');
-        return;
-    }
-
     const base = prompt('Введите систему счисления от 2 до 36');
 
-    if (!isNumValid(base)) {
-        console.log('Некорректный ввод!');
-        return;
+    if (!isNumValid(num) || !isNumValid(base) || base < 1 || base > 37) {
+        return 'Некорректный ввод!';  
     }
 
     const result = convertNumber(+num, +base);
-    console.log(result);
+    return result
 }
 
-convertNumberToBase();
+console.log(convertNumberToBase());
