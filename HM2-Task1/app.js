bf1f70b3d2193bcef88502c2eb898a90114c6519
andreirysh <andreirysh@gmail.com>
2023-08-06 23:41:02 +0200

    Update app.js

--- HM2-Task1/app.js
+++ HM2-Task1/app.js
@@ -1,43 +1,43 @@
-// function makeObjectDeepCopy(obj) {
-//     if (typeof obj !== 'object' || obj === null) {
-//         return obj;
-//     }
+function makeObjectDeepCopy(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return obj;
+    }
 
-//     const copyOfObj = Array.isArray(obj) ? [] : {};
+    const copyOfObj = Array.isArray(obj) ? [] : {};
 
-//     for (let key in obj) {
+    for (let key in obj) {
 
-//         if (Object.prototype.hasOwnProperty.call(obj, key)) {
-//             copyOfObj[key] = makeObjectDeepCopy(obj[key]);
-//         }
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            copyOfObj[key] = makeObjectDeepCopy(obj[key]);
+        }
 
-//     }
-//     return copyOfObj;
-// }
+    }
+    return copyOfObj;
+}
 
 
-// function selectFromInterval(array, firstInterval, secondInterval) {
+function selectFromInterval(array, firstInterval, secondInterval) {
 
-//     let startInterval, endInterval;
+    let startInterval, endInterval;
 
-//     const isNumInvalid = !array.every((num) => typeof num === 'number') || typeof firstInterval !== 'number' || typeof secondInterval !== 'number';
+    const isNumInvalid = !array.every((num) => typeof num === 'number') || typeof firstInterval !== 'number' || typeof secondInterval !== 'number';
 
-//     if (isNumInvalid) {
-//         throw new Error('Ошибка!');
-//     }
+    if (isNumInvalid) {
+        throw new Error('Ошибка!');
+    }
 
-//     if (firstInterval < secondInterval) {
-//         startInterval = firstInterval;
-//         endInterval = secondInterval;
-//     }
+    if (firstInterval < secondInterval) {
+        startInterval = firstInterval;
+        endInterval = secondInterval;
+    }
 
-//     else {
-//         startInterval = secondInterval;
-//         endInterval = firstInterval;
-//     }
+    else {
+        startInterval = secondInterval;
+        endInterval = firstInterval;
+    }
 
-//     return array.filter((num) => num >= startInterval && num <= endInterval);
-// }
+    return array.filter((num) => num >= startInterval && num <= endInterval);
+}
 
 const myIterable = {
     from: 1,
