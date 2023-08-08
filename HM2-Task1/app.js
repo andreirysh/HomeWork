5349f628b0bacaac8f57784c1bd88c6bd8cfee84
andreirysh <andreirysh@gmail.com>
2023-08-08 10:39:50 +0200

    Update app.js

--- HM2-Task1/app.js
+++ HM2-Task1/app.js
@@ -6,18 +6,15 @@ function makeObjectDeepCopy(obj) {
     const copyOfObj = Array.isArray(obj) ? [] : {};
 
     for (let key in obj) {
-
         if (Object.prototype.hasOwnProperty.call(obj, key)) {
             copyOfObj[key] = makeObjectDeepCopy(obj[key]);
         }
-
     }
     return copyOfObj;
 }
 
 
 function selectFromInterval(array, firstInterval, secondInterval) {
-
     let startInterval, endInterval;
 
     const isNumInvalid = !array.every((num) => typeof num === 'number') || typeof firstInterval !== 'number' || typeof secondInterval !== 'number';
@@ -45,6 +42,7 @@ const myIterable = {
     [Symbol.iterator]() {
         let from = this.from;
         let to = this.to;
+
         const isNumInvalid = typeof this.from !== 'number' || typeof this.to !== 'number' || this.to < this.from
 
         if (isNumInvalid) {
