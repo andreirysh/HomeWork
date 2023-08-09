4fa1ce704e4c714715eeb18af53869bb22c05a46
andreirysh <andreirysh@gmail.com>
2023-08-09 19:12:59 +0200

    HM3 Done

---
+++ app.js
@@ -0,0 +1,27 @@
+Array.prototype.myFilter = function (callbackFn, context) {
+    const filteredArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+        const result = callbackFn.call(context, this[i], i, this);
+
+        if (result) {
+            filteredArr.push(this[i]);
+        }
+    }
+    return filteredArr;
+};
+
+function createDebounceFunction(callbackFn, delayInMs) {
+    let timeoutID;
+
+    return function () {
+        clearTimeout(timeoutID);
+
+        timeoutID = setTimeout(() => {
+            callbackFn();
+        }, delayInMs);
+    }
+}
+
+
+
